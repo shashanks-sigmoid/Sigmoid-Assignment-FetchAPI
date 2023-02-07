2cccbd5a82f7c409306985eb1f0b0901998c876d
shashanks-sigmoid <shashanks@sigmoidanalytics.com>
2023-02-08 01:07:34 +0530

    task done

--- frontend/src/components/FetchData.jsx
+++ frontend/src/components/FetchData.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react'
 import axios from 'axios'
 import { useState } from 'react'
+import "../stylesheets/fetchData.css"
 //require("dotenv").config();
 
 function FetchData() {
 
     const SHEET_ID = "1Q-B2btkfurhJUQriLhJaagiDJB5foh-hUjToX21yWXE"
-    const PRIVATE_KEY = process.env.REACT_APP_PRIVATE_KEY
+    const PRIVATE_KEY = 'ya29.a0AVvZVsrlQpOlrp1dyMT-NZid4vB40gpJDWmdUTW_aI4RqlOgVFUkWkTZg9oF87wqkSF2SS7X7ErRfKsz2Nn5RLz2FnHnyujwCwP_9j3xf10spbBPLQr1o5vaFrCu5l8TJDwdOYGz1_CPEJmw3oPCKAJw6wjTaCgYKAZsSARISFQGbdwaIMP2wGL0kgUqgLlmwEqweRA0163'
+
     const [fetchData, setFetchData] = useState([])
 
     useEffect(() => {
@@ -19,13 +21,13 @@ function FetchData() {
         axios
             .get(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/A1:F31`, {
                 headers: {
-                    // "Content-Type": "application/json",
+                    "Content-Type": "application/json",
                     Authorization: `Bearer ${PRIVATE_KEY}`,
                 }
             })
             .then((result) => {
-                console.log(result)
-                setFetchData(result)
+                setFetchData(result.data.values)
+                console.log(fetchData)
             })
             .catch((err) => {
                 console.log(err.message)
@@ -35,6 +37,26 @@ function FetchData() {
     return (
         <div>
             <h1>Google Sheet Fetch API</h1>
+            <table className='table'>
+                <thead className='columns'>
+                    <tr>
+                        {fetchData[0]?.map((col, i) => {
+                            return <th className='header' key={i}>{col}</th>
+                        })}
+                    </tr>
+                </thead>
+                <tbody className='rows'>
+                    {fetchData.slice(1).map((rows, key) => {
+                        return (
+                            <tr key={key}>
+                                {rows.map((row, i) => {
+                                    return <td className='element' key={i}>{i == 2 ? row.slice(3) : row}</td>
+                                })}
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
         </div>
     )
 }
